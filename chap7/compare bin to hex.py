12cd329d1e51bac05b99e43e5214ba7586125221
ysravya <syellapragada@hyperiongrp.com>
2019-08-27 20:48:05 +0530

    chapter 7 examples

--- chap7/compare bin to hex.py
+++ chap7/compare bin to hex.py
@@ -1,1 +1,41 @@
 '''write a function to check if the value of a binary number (passed as a string) equals the hexadecimal representation of a string.'''
+class BintoHex():
+    def convertBinToHex(self,binary,hexa):
+        num1 = self.convertToDecimal(binary,2)
+        print(num1)
+        num2 = self.convertToDecimal(hexa,16)
+        print(num2)
+        if(num1 == num2):
+            print("matched")
+        elif((num1 == -1) or (num2 == -1)):
+            print("Invalid format")
+        else:
+            print("Not matched")
+
+    def convertToDecimal(self,representation,base):
+        if(base < 2 or (base>10 and base != 16)):
+            return -1
+        strlen = len(representation)-1
+        hexdecmap = {'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
+        sum = 0
+        for char in representation:
+            try:
+                if (int(char) < base and int(char) >= 0):
+                    sum += int(char)*pow(base,strlen)
+                else:
+                    return -1
+            except:
+                if(char.upper() in hexdecmap.keys()):
+                    sum += hexdecmap[char]*pow(base,strlen)
+                else:
+                    print("invalid hexadecimal number")
+                    return -1
+            strlen -= 1
+
+        return(sum)
+
+
+if __name__ == '__main__':
+    bin_num = '1010'
+    hex_num = 'A'
+    BintoHex().convertBinToHex(bin_num,hex_num)
--- chap7/find median in a random array.py
+++ chap7/find median in a random array.py
@@ -1,1 +1,15 @@
 '''Numbers are randomly generated and stored into an (expanding) array. How would you keep track of the median?'''
+from QuickSort import *
+
+class FindMedian:
+    def findMedian(self,arr):
+        QuickSort().quickSort(arr,0,len(arr)-1)
+        if(len(arr)%2 == 0):
+            median = (arr[int(len(arr)/2)]+arr[int((len(arr)-1)/2)])/2
+        else:
+            median = arr[int(len(arr/2))]
+        print(median)
+
+if __name__ == "__main__":
+    arr = [1,0,3,4,5,2]
+    FindMedian().findMedian(arr)
