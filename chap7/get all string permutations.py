f87c07c71cec513ec12a5c3c588dc69651be7647
ysravya <syellapragada@hyperiongrp.com>
2019-08-29 18:11:06 +0530

    29th Aug

--- chap7/get all string permutations.py
+++ chap7/get all string permutations.py
@@ -12,24 +12,45 @@ class Permutations:
         return ''.join(c)
 
     def swap_chars_str(self,str,k):
-        print("input arr: ",str)
-        print("level :", k)
+        #print("input arr: ",str)
+        #print("level :", k)
         str_gen = []
         for i in range(k,len(str)):
             temp_str = str
-            result = self.swap(temp_str,i,0)
+            result = self.swap(temp_str,i,k)
             str_gen.append(result)
-        print("permutations: ",str_gen)
+        #print("permutations: ",str_gen)
         return str_gen
 
-    def stringPermutations(self,generator,l):
-        #num_perm = self.factorial(l)
-        #print("No of permutations possible assuming all chars are unique are ",num_perm)
-        for i in generator:
-            result = self.swap_chars_str(i,l-l)
-        self.stringPermutations(result,l-1)
+    def computeFactorial(self,num):
+        res = [1]
+        while(num > 1):
+            res.append(num*res[-1])
+            num -= 1
+        #print(res)
+        new_res = []
+        for i in range(0,len(res)):
+            new_res.append(sum(res[:i+1]))
+        #print(new_res)
+        return new_res
+
+    def stringPermutations(self,str,l,k):
+        facts = self.computeFactorial(l)
+        count = 0
+        for ind in str:
+            if(count in facts):
+                k = k+1
+                if(k==facts[-1]):
+                    break
+            res = self.swap_chars_str(ind,l-(l-k))
+            for i in res:
+                str.append(i)
+            count += 1
+        strlist = sorted(list(set(str)))
+        print("String Permutations are: ",(",".join(s for s in strlist)))
+        print("No of permutations are :",len(strlist))
+
 
 if __name__ == "__main__":
-    str = ['abcd']
-    l = len(str[0])
-    Permutations().stringPermutations(str,l)
+    str = 'abcd'
+    Permutations().stringPermutations([str],len(str),0)
